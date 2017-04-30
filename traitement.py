6067f734cc6859edb9a164932dac6b6cde0dad76
jonathanberthias <jvberthias@gmail.com>
2017-04-30 14:56:54 +0200

    Ajout de la prise en compte des notes

--- traitement.py
+++ traitement.py
@@ -29,6 +29,7 @@ class AssociateurCommentairesFilms:
         """
         self.comment_film = {}
         self.film_titre = {}
+        self.notes = {}
         if not os.path.exists(path_to_index):
             raise IOError("Pas d'index au chemin: %s" % path_to_index)
         print("Chemin vers l'index: %s" % path_to_index)
@@ -88,7 +89,7 @@ class TraiteurCommentaire:
         return debut + fin
 
     def _enlever_ponctuation(self, comment):
-        """Nettoie la ponctuation."""
+        """Nettoie la ponctuation et remplace par des espaces."""
         return self.pattern.sub(' ', comment.lower())
 
     def _lemmatiser(self, mots):
@@ -99,20 +100,21 @@ class TraiteurCommentaire:
 class EcriveurFichiersFilms:
     """Crée les fichiers des films."""
 
-    def __init__(self, path_to_folder):
+    def __init__(self, path_to_films, path_to_moyennes):
         """Crée le dossier pour stocker les films.
 
         Renvoie 1 si un nouveau fichier film a été crée, 0 sinon.
         """
-        self.chemin = path_to_folder
-        if os.path.exists(self.chemin):
-            # On importe que si besoin
-            shutil.rmtree(self.chemin)
-        os.makedirs(self.chemin)
+        self.chemin_films = path_to_films
+        if os.path.exists(self.chemin_films):
+            # On supprime les fichiers qui existent déjà
+            shutil.rmtree(self.chemin_films)
+        os.makedirs(self.chemin_films)
+        self.chemin_moyennes = path_to_moyennes
 
     def ecrire_commentaire(self, comment, film_id):
         """Ecrit le commentaire pour le film donné."""
-        fichier = os.path.join(self.chemin, film_id)
+        fichier = os.path.join(self.chemin_films, film_id)
         if os.path.exists(fichier):
             # 'utf8' pour éviter les problèmes d'encodage/décodage
             with open(fichier, 'a', encoding='utf8') as film:
@@ -124,33 +126,47 @@ class EcriveurFichiersFilms:
                 film.write(comment)
             return 1
 
+    def ecrire_moyennes(self, moyennes):
+        """Ecrire un fichier pour stocker la note moyenne de chaque film."""
+        with open(self.chemin_moyennes, 'w', encoding='utf8') as fichier:
+            for film, moyenne in moyennes.items():
+                fichier.write("%s:%.2f\n" % (film, moyenne))
+
 
 class Traitement:
     """Traite les commentaires et les range nettoyés dans le bon fichier."""
 
-    def __init__(self, path_to_comments, path_to_films):
+    def __init__(self, path_to_comments, path_to_films, path_to_moyennes):
         """Initialise le traiteur.
 
         :param path_to_comments: chemin vers les commentaires
         :param path_to_films: chemin vers les films
+        ;param path_to_moyennes: chemin vers le fichier de moyennes
         """
         self.path_to_comments = path_to_comments
         self.path_to_films = path_to_films
+        self.path_to_moyennes = path_to_moyennes
         print("Chemin vers les commentaires: %s" % self.path_to_comments)
         print("Chemin vers les films: %s" % self.path_to_films)
+        print("Chemin vers les moyennes: %s" % self.path_to_moyennes)
 
     def traiter(self, nb_com=25000, progress=True, associateur=None):
         """Effectue l'ensemble du traitement pour tous les commentaires."""
-        # Variables pour les fonctions pour gagner un peu de temps.
         traiteur = TraiteurCommentaire()
-        writer = EcriveurFichiersFilms(self.path_to_films)
+        writer = EcriveurFichiersFilms(self.path_to_films,
+                                       self.path_to_moyennes)
+        notes = {}
+        notes_moyennes = {}
 
         num_com = 0
         num_films = 0
         print("Ecriture des fichiers film.")
         debut = time.time()
         for com in os.listdir(self.path_to_comments):
-            com_id = com[:com.find("_")]
+            sep = com.find("_")
+            com_id = com[:sep]
+            # Note entre 1 et 10
+            note = com[sep + 1:com.find('.')]
             num_com += 1
             if num_com > nb_com:
                 break
@@ -164,6 +180,12 @@ class Traitement:
                 commentaire = traiteur.traiter_commentaire(commentaire)
                 film_id = associateur.get_film(com_id)
                 num_films += writer.ecrire_commentaire(commentaire, film_id)
+                notes[film_id] = notes.get(film_id, []) + [note]
+        for film, notes_film in notes.items():
+            nb_com_film = len(notes_film)
+            moyenne = sum(map(int, notes_film)) / nb_com_film
+            notes_moyennes[film] = moyenne
+        writer.ecrire_moyennes(notes_moyennes)
         print("\n%d commentaires traités et %d fichiers film créés en %.3fs." %
               (nb_com, num_films, (time.time() - debut)))
-        return associateur
+        return notes_moyennes
